ea92b880c42a2022c0be6bf1855a07db5704113d
HongSu-Kim <coldghd@gmail.com>
2022-06-03 18:39:24 +0900

    220603

--- src/com/day6/P187_Student.java
+++ src/com/day6/P187_Student.java
@@ -1,15 +1,17 @@
 package com.day6;
 
+// static
+
 public class P187_Student {
-	private static int serialNum = 1000;
+	private static int serialNum = 1000; // static(클래스) 변수 : 클래스에서 공통으로 사용 하는 변수, 모든 인스턴스가 공유
 	int studentID;
 	String studentName;
 	int grade;
 	String address;
 
 	public P187_Student() {
-		serialNum++;
-		studentID = serialNum;
+		serialNum++; // 학생이 생성될 때마다 증가
+		studentID = serialNum; // 증가된 값을 인스턴스 변수에 부여
 	}
 
 	public String getStudentName() {
@@ -20,13 +22,15 @@ public class P187_Student {
 		studentName = name;
 	}
 
-	public static int getSerialNum() {
-//		int i = 10;
-		return serialNum;
+	public static int getSerialNum() { // static(클래스) 메소드
+		@SuppressWarnings("unused")
+		int i = 10; // 지역변수는 사용가능
+		// studentName = "이지원"; // 멤버(인스턴스) 변수는 사용할수없다.
+		return serialNum; // static 변수 사용가능
 	}
-	
+
 	public static void setSerialNum(int serialNum) {
 		P187_Student.serialNum = serialNum;
 	}
-	
+
 }
--- src/com/day6/P188_StudentTest.java
+++ src/com/day6/P188_StudentTest.java
@@ -5,8 +5,9 @@ public class P188_StudentTest {
 	public static void main(String[] args) {
 
 		P187_Student studentLee = new P187_Student();
-		studentLee.setStudentName("이지원");
-		System.out.println(P187_Student.getSerialNum());
+		studentLee.setStudentName("이지원"); 
+		//System.out.println(P187_Student.serialNum); // 접근제어자가 private이기 때문에 get() 메소드를 통해 불러와야한다.
+		System.out.println(P187_Student.getSerialNum()); // static 변수는 클래스 이름으로 직접 참조한다.
 		System.out.println(studentLee.studentName + " 학번 : " + studentLee.studentID);
 
 		P187_Student studentSon = new P187_Student();
